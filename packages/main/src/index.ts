9c8425ad3cca0b0af91556be530983b26a09a641
utkusarioglu <utkusarioglu@hotmail.com>
2020-12-28 21:08:52 +0300

    Feat: add body parser

--- packages/main/src/index.ts
+++ packages/main/src/index.ts
@@ -3,6 +3,7 @@ require('dotenv').config({
 });
 import express from 'express';
 import cors from 'cors';
+import bodyParser from 'body-parser';
 
 const app = express();
 app.use(
@@ -10,6 +11,8 @@ app.use(
     origin: 'http://192.168.1.152',
   })
 );
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
 
 app.listen(HTTP_PORT, () => {
   console.log(`App started on: ${HTTP_PORT} - ${store.value}`);
