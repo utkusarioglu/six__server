4e9811ca7e66d33bcf69b2ef25c21cbfe3bb1b86
utkusarioglu <utkusarioglu@hotmail.com>
2020-12-28 21:18:08 +0300

    Feat: implement auth in main

--- packages/main/src/index.ts
+++ packages/main/src/index.ts
@@ -6,6 +6,9 @@ import cors from 'cors';
 import bodyParser from 'body-parser';
 import { HTTP_PORT, ALLOWED_ORIGINS } from './config';
 import helmet from 'helmet';
+import { useAuth, checkAuth } from 'six__server__auth';
+import insecure from './routes/insecure';
+import secure from './routes/secure';
 import fourOFour from './routes/404';
 
 const app = express();
@@ -17,6 +20,10 @@ app.use(
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(helmet());
+useAuth(app);
+
+app.use('/api', insecure);
+app.use('/api/auth', checkAuth, secure);
 app.use(fourOFour);
 
 app.listen(HTTP_PORT, () => {
