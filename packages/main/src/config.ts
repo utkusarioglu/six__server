405445588c5ad1c7c9440903997b620db824ee23
utkusarioglu <utkusarioglu@hotmail.com>
2020-12-28 21:17:17 +0300

    Feat: add allowed origins to config

--- packages/main/src/config.ts
+++ packages/main/src/config.ts
@@ -1,6 +1,9 @@
-const { NODE_HTTP_PORT } = process.env;
+const { NODE_HTTP_PORT, NODE_ALLOWED_ORIGINS } = process.env;
 
 if (!NODE_HTTP_PORT)
-  throw new Error('NODE_HTTP_PORT is a required environment variable');
+  throw new Error('.env/NODE_HTTP_PORT is a required by main package');
+if (!NODE_ALLOWED_ORIGINS)
+  throw new Error('.env/NODE_ALLOWED_ORIGINS is required by main package');
 
 export const HTTP_PORT = NODE_HTTP_PORT;
+export const ALLOWED_ORIGINS = NODE_ALLOWED_ORIGINS.split(',');
