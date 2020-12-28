e5d14834debce833c01b1c40654f18cbe5f1876b
utkusarioglu <utkusarioglu@hotmail.com>
2020-12-28 21:16:15 +0300

    Feat: implement basic auth

---
+++ packages/auth/src/index.ts
@@ -0,0 +1,109 @@
+require('dotenv').config({
+  path: require('path').join(__dirname, '../../../.env'),
+});
+import Passport from 'passport';
+import passportLocal from 'passport-local';
+import expressSession from 'express-session';
+import store from 'six__server__store';
+import { SESSION_SECRET } from './config';
+import type { Express, Request, Response, NextFunction } from 'express';
+
+const LocalStrategy = passportLocal.Strategy;
+
+Passport.use(
+  'local',
+  new LocalStrategy(
+    {
+      usernameField: 'username',
+      passwordField: 'password',
+    },
+    async (username, password, done) => {
+      try {
+        const user = await store.login.withUsernameAndPassword(
+          username,
+          password
+        );
+        if (user) {
+          done(null, user);
+        } else {
+          done(null, false);
+        }
+      } catch (e) {
+        done(e);
+      }
+    }
+  )
+);
+
+Passport.serializeUser(async (user, done) => {
+  try {
+    // @ts-ignore
+    const serializedId = await store.login.serializeUser(user);
+    done(null, serializedId);
+  } catch (e) {
+    done(e);
+  }
+});
+
+Passport.deserializeUser(async (id, done) => {
+  try {
+    // @ts-ignore
+    const user = await store.login.deserializeUser(id);
+    if (user) {
+      done(null, user);
+    } else {
+      done(false);
+    }
+  } catch (e) {
+    done(e);
+  }
+});
+
+export function useAuth(app: Express) {
+  app.use(
+    expressSession({
+      secret: SESSION_SECRET,
+    })
+  );
+  app.use(Passport.initialize());
+  app.use(Passport.session());
+
+  app.post('/api/login', (req, res, next) => {
+    passport.authenticate('local', (err, user, info) => {
+      if (err) {
+        return next(err);
+      }
+
+      if (!user) {
+        return res.send({ message: 'fail' });
+      }
+
+      req.login(user, (err) => {
+        if (err) {
+          console.warn(`login error:\n${err}`);
+          return next(err);
+        }
+        return res.send({ message: 'login success' });
+      });
+    })(req, res, next);
+  });
+
+  app.post('/api/logout', (req, res, next) => {
+    if (req.user) {
+      req.logout();
+      next('logged out');
+    } else {
+      next("wasn't logged in");
+    }
+  });
+}
+
+export function checkAuth(req: Request, res: Response, next: NextFunction) {
+  if (req.isAuthenticated()) {
+    next();
+  }
+  // TODO this shall be connected to somewhere more meaningful
+  res.redirect('/api/headers');
+}
+
+export const passport = Passport;
