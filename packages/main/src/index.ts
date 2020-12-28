8d05d8079201a686e0994302d3554b4853784dc7
utkusarioglu <utkusarioglu@hotmail.com>
2020-12-28 21:08:20 +0300

    Feat: add cors

--- packages/main/src/index.ts
+++ packages/main/src/index.ts
@@ -2,13 +2,14 @@ require('dotenv').config({
   path: require('path').join(__dirname, '../../../.env'),
 });
 import express from 'express';
-import { HTTP_PORT } from './config';
-import { store } from 'six__server__store';
+import cors from 'cors';
 
 const app = express();
-app.get('/', (_, res) => {
-  res.send('hi');
-});
+app.use(
+  cors({
+    origin: 'http://192.168.1.152',
+  })
+);
 
 app.listen(HTTP_PORT, () => {
   console.log(`App started on: ${HTTP_PORT} - ${store.value}`);
