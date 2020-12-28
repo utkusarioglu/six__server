6fc05b3cff037cc8cdf800ffb669fc538d7e056f
utkusarioglu <utkusarioglu@hotmail.com>
2020-12-28 21:11:57 +0300

    Feat: use config vars in main

--- packages/main/src/index.ts
+++ packages/main/src/index.ts
@@ -4,16 +4,17 @@ require('dotenv').config({
 import express from 'express';
 import cors from 'cors';
 import bodyParser from 'body-parser';
+import { HTTP_PORT, ALLOWED_ORIGINS } from './config';
 
 const app = express();
 app.use(
   cors({
-    origin: 'http://192.168.1.152',
+    origin: ALLOWED_ORIGINS,
   })
 );
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
 app.listen(HTTP_PORT, () => {
-  console.log(`App started on: ${HTTP_PORT} - ${store.value}`);
+  console.log(`App started on: ${HTTP_PORT}`);
 });
