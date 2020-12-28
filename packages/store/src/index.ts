09323a1d5ebd1d491d46d54242809c6c48362111
utkusarioglu <utkusarioglu@hotmail.com>
2020-12-28 21:18:26 +0300

    Feat: Implement auth store access

--- packages/store/src/index.ts
+++ packages/store/src/index.ts
@@ -1,3 +1,49 @@
-export const store = {
-  value: 'hello from store',
+import type { User } from 'six__server__auth/src/@types/user';
+
+const store = {
+  login: {
+    /**
+     * Used by passport local strategy to login users
+     * @param username
+     * @param password
+     */
+    withUsernameAndPassword: async (username: string, password: string) => {
+      if (username === 'utku' && password === '1') {
+        return {
+          id: 'utku',
+          name: 'utku',
+          motto: 'omg is this real?',
+        };
+      } else {
+        false;
+      }
+    },
+
+    /**
+     * Serializes the session info to database. Used by passport to serialize
+     * user information for the session
+     * @param user user map defined by {@link User}
+     */
+    serializeUser: async (user: User) => {
+      serialStore[user.id] = user;
+      return Promise.resolve(user.id);
+    },
+
+    /**
+     * Returns the user info for the id given. Used by passport to
+     * deserialize the session
+     * @param id user.id from {@link User}
+     */
+    deserializeUser: async (id: string) => {
+      return Promise.resolve(serialStore[id]);
+    },
+  },
+
+  test: {
+    string: () => 'store.test.string',
+  },
 };
+
+const serialStore: { [id: string]: User } = {};
+
+export default store;
