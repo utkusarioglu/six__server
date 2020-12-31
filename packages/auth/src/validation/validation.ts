986ea25aa901f836641703369332857f6adac94e
utkusarioglu <utkusarioglu@hotmail.com>
2020-12-31 21:54:24 +0300

    Feat: add new signup validation functions

--- packages/auth/src/validation/validation.ts
+++ packages/auth/src/validation/validation.ts
@@ -31,3 +31,33 @@ export function passwordLengthValid(password: string): boolean {
     max: PASSWORD_LENGTH_MAX,
   });
 }
+
+/**
+ * Checks whether the password given is up to the strength standards
+ * This function doesn't check length because length check is handled
+ * by {@link passwordLengthValid}.
+ * @param password password to check
+ */
+export function passwordStrengthAcceptable(password: string): boolean {
+  return validator.isStrongPassword(password, {
+    minLowercase: 1,
+    minUppercase: 1,
+    minNumbers: 1,
+  });
+}
+
+/**
+ * Checks whether the given email string is valid
+ * @param email email string to check
+ */
+export function isValidEmail(email: string): boolean {
+  return validator.isEmail(email);
+}
+
+/**
+ * Checks if the given number is within reasonable human age limits
+ * @param age age to check
+ */
+export function isValidAge(age: number): boolean {
+  return 0 < age && age < 120;
+}
