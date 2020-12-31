e60f74b2f59fff67e1e8918513a4f23ad156b6aa
utkusarioglu <utkusarioglu@hotmail.com>
2020-12-31 19:59:09 +0300

    Feat: define username and passworrd env types

--- packages/auth/src/@types/node.d.ts
+++ packages/auth/src/@types/node.d.ts
@@ -2,6 +2,10 @@ declare global {
   namespace NodeJS {
     interface ProcessEnv {
       SERVER_SESSION_SECRET: string;
+      AUTH_USERNAME_LENGTH_MIN: string; // number
+      AUTH_USERNAME_LENGTH_MAX: string; // number
+      AUTH_PASSWORD_LENGTH_MIN: string; // number
+      AUTH_PASSWORD_LENGTH_MAX: string; // number
     }
   }
 }
