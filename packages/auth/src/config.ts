59cfca2408cb4840d9216ae7c06a6de550378fdb
utkusarioglu <utkusarioglu@hotmail.com>
2020-12-31 19:56:25 +0300

    Feat: add username, pass accepted limits as env

--- packages/auth/src/config.ts
+++ packages/auth/src/config.ts
@@ -1,6 +1,28 @@
-const { SERVER_SESSION_SECRET } = process.env;
+const {
+  SERVER_SESSION_SECRET,
+  AUTH_USERNAME_LENGTH_MIN,
+  AUTH_USERNAME_LENGTH_MAX,
+  AUTH_PASSWORD_LENGTH_MIN,
+  AUTH_PASSWORD_LENGTH_MAX,
+} = process.env;
 
 if (!SERVER_SESSION_SECRET)
   throw new Error('.env/NODE_SESSION_SECRET is required by auth package');
 
+if (!AUTH_USERNAME_LENGTH_MIN)
+  throw new Error('.env/AUTH_USERNAME_LENGTH_MIN is required by auth package');
+
+if (!AUTH_USERNAME_LENGTH_MAX)
+  throw new Error('.env/AUTH_USERNAME_LENGTH_MAX is required by auth package');
+
+if (!AUTH_PASSWORD_LENGTH_MIN)
+  throw new Error('.env/AUTH_PASSWORD_LENGTH_MIN is required by auth package');
+
+if (!AUTH_PASSWORD_LENGTH_MAX)
+  throw new Error('.env/AUTH_PASSWORD_LENGTH_MAX is required by auth package');
+
 export const SESSION_SECRET = SERVER_SESSION_SECRET;
+export const USERNAME_LENGTH_MIN = +AUTH_USERNAME_LENGTH_MIN;
+export const USERNAME_LENGTH_MAX = +AUTH_USERNAME_LENGTH_MAX;
+export const PASSWORD_LENGTH_MIN = +AUTH_PASSWORD_LENGTH_MIN;
+export const PASSWORD_LENGTH_MAX = +AUTH_PASSWORD_LENGTH_MAX;
