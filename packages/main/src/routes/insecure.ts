45b9051229a745a735a2183c0c35972a0902c064
utkusarioglu <utkusarioglu@hotmail.com>
2021-01-03 20:37:39 +0300

    Feat: expand headers route return

--- packages/main/src/routes/insecure.ts
+++ packages/main/src/routes/insecure.ts
@@ -3,7 +3,16 @@ import express from 'express';
 const router = express.Router();
 
 router.get('/headers', (req, res) => {
-  res.json({ route: 'headers', headers: req.headers });
+  res.json({
+    route: 'headers',
+    headers: req.headers,
+    ip: req.ip,
+    hostname: req.hostname,
+    protocol: req.protocol,
+    resHeaders: res.getHeaders(),
+    reqCookies: req.cookies,
+    session: req.session,
+  });
 });
 
 export default router;
