ef09ed31e7841ff71f405302b57fb16c650b70ef
utkusarioglu <utkusarioglu@hotmail.com>
2021-01-11 15:23:48 +0300

    Feat: update mock-data with new models

--- packages/mock-data/src/index.ts
+++ packages/mock-data/src/index.ts
@@ -1,24 +1,74 @@
 import store from 'six__server__store';
+import {
+  USERS,
+  COMMUNITIES,
+  POSTS,
+  VISITOR_COMMUNITIES,
+  USER_COMMUNITY_SUBSCRIPTIONS,
+} from './constants';
 
 async function mockUsers(): Promise<void> {
-  await store.auth.clearUsers();
-  return await store.auth.insertUsers([
-    {
-      user_id: 'fced7bf2-fa80-46fe-b83b-0cae45ca5db0',
-      username: 'utkusarioglu',
-      password: '$2b$10$7O1nkhhxET9jgLY3FvAWseEgM5qELRtqeqlef051y3r9FD/XTwUZC',
-      email: 'utkusarioglu@gmail.com',
-      age: 36,
-    },
-  ]);
+  await store.user.deleteAll();
+  await store.user.insert(USERS);
+}
+
+async function mockCommunities(): Promise<void> {
+  await store.community.deleteAll();
+  await store.community.insert(COMMUNITIES);
+
+  await store.visitorCommunitySubscription.deleteAll();
+  await store.visitorCommunitySubscription.insert(VISITOR_COMMUNITIES);
+
+  await store.userCommunitySubscription.insert(USER_COMMUNITY_SUBSCRIPTIONS);
+}
+
+async function mockComments() {
+  await store.comment.deleteAll();
+}
+
+async function mockVotes() {
+  await store.vote.deleteAll();
 }
 
 /**
- * Creates mock data for development purposes.
+ * Creates mock posts library
+ */
+async function mockPosts(): Promise<void> {
+  // await store.post.deleteAll();
+
+  await POSTS.reduce((chain, post) => {
+    chain = chain.then(() => {
+      store.post.insert(post);
+    });
+    return chain;
+  }, Promise.resolve());
+}
+
+/***
+ * Clears n-ary associations from the store
+ */
+async function clearNaryAssociations() {
+  await store.commentVote.deleteAll();
+  await store.communityPost.deleteAll();
+  await store.postVote.deleteAll();
+  await store.userComment.deleteAll();
+  await store.userCommunityCreator.createTable();
+  await store.userCommunitySubscription.deleteAll();
+  await store.userPost.deleteAll();
+  await store.userVote.deleteAll();
+}
+
+/**
+ * Creates mock data for development purposes
  *
  * @privateRemarks
  * This function should not be called in production
  */
 export async function createMockData(): Promise<void> {
-  return await mockUsers();
+  // await clearNaryAssociations();
+  await mockUsers();
+  await mockCommunities();
+  await mockVotes();
+  await mockPosts();
+  await mockComments();
 }
