0aa929af1479c0b52df313db05b2697a173fd2a6
utkusarioglu <utkusarioglu@hotmail.com>
2021-01-11 15:24:16 +0300

    Feat: add posts route to main

--- packages/main/src/routes/insecure.ts
+++ packages/main/src/routes/insecure.ts
@@ -1,4 +1,5 @@
 import express from 'express';
+import store from 'six__server__store';
 
 const router = express.Router();
 
@@ -13,6 +14,17 @@ router.get('/headers', (req, res) => {
     reqCookies: req.cookies,
     session: req.session,
   });
+});
+
+router.get('/posts', async (req, res) => {
+  const posts =
+    req.isAuthenticated() && req.user.id
+      ? await store.post.selectUserPosts(req.user.id)
+      : await store.post.selectVisitorPosts();
+
+  console.group('user id\n', req.user, '\n', posts);
+
+  res.json(posts);
 });
 
 export default router;
