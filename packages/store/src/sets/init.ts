b18324467ce01fcb92fe7231bad851035f9a8a79
utkusarioglu <utkusarioglu@hotmail.com>
2021-01-11 15:21:05 +0300

    Feat: enhance init function for store
    - Init function has received the initialization steps for new commits

--- packages/store/src/sets/init.ts
+++ packages/store/src/sets/init.ts
@@ -1,6 +1,28 @@
 import type Store from '../index';
 
+/**
+ * Initializes everything that the app needs for the store to function
+ * @param store store object that contains access to all data sources
+ */
 export async function initStore(store: typeof Store): Promise<void> {
-  return await store.auth.initUsers();
-  // return await store.auth.createSessions();
+  // entities
+  await store.user.createTable();
+  await store.comment.createTable();
+  await store.post.createTable();
+  await store.community.createTable();
+  await store.vote.createTable();
+
+  // associations
+  await store.commentVote.createTable();
+  await store.communityPost.createTable();
+  await store.postComment.createTable();
+  await store.postVote.createTable();
+  await store.userComment.createTable();
+  await store.userCommunityCreator.createTable();
+  await store.userCommunitySubscription.createTable();
+  await store.userPost.createTable();
+  await store.userVote.createTable();
+  await store.visitorCommunitySubscription.createTable();
+
+  return;
 }
