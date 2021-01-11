bb9002d1a7bc7d074d066756a9d9555ac6bb212f
utkusarioglu <utkusarioglu@hotmail.com>
2021-01-11 15:23:07 +0300

    Feat: add new models to store object

--- packages/store/src/index.ts
+++ packages/store/src/index.ts
@@ -1,13 +1,42 @@
-import auth from './methods/auth';
 import { initStore } from './sets/init';
 import { sessionConnector } from './connectors/session';
-import postgres from './connectors/knex';
+import postgres from './connectors/postgres';
+
+import user from './models/user/user';
+import comment from './models/comment/comment';
+import post from './models/post/post';
+import community from './models/community/community';
+import commentVote from './models/comment-vote/comment-vote';
+import communityPost from './models/community-post/community-post';
+import postComment from './models/post-comment/post-comment';
+import postVote from './models/post-vote/post-vote';
+import userComment from './models/user-comment/user-comment';
+import userCommunityCreator from './models/user-community-creator/user-community-creator';
+import userCommunitySubscription from './models/user-community-subscription/user-community-subscription';
+import userPost from './models/user-post/user-post';
+import userVote from './models/user-vote/user-vote';
+import vote from './models/vote/vote';
+import visitorCommunitySubscription from './models/visitor-community-subscription/visitor-community-subscription';
 
 /**
  * Single source of store actions
  */
 const store = {
-  auth,
+  user,
+  comment,
+  post,
+  community,
+  commentVote,
+  communityPost,
+  postComment,
+  postVote,
+  userComment,
+  userCommunityCreator,
+  userCommunitySubscription,
+  userPost,
+  userVote,
+  vote,
+  visitorCommunitySubscription,
 
   /**
    * Data sources that the store is using. This object gives access to
