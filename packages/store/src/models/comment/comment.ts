ab032bea1724a3b0c1d5353c92ce4370a5039c13
utkusarioglu <utkusarioglu@hotmail.com>
2021-02-05 11:33:28 +0300

    Feat: add custom insert method to CommentStore
    * Comment has a lot associatiion to update when a record is inserted.
     This method handles all of these

--- packages/store/src/models/comment/comment.ts
+++ packages/store/src/models/comment/comment.ts
@@ -1,3 +1,4 @@
+import { uuid } from '../../@types/helpers';
 import postgres from '../../connectors/postgres';
 import { Model } from '../model/model';
 import { CommentInsert, CommentModel } from './comment.types';
@@ -29,6 +30,73 @@ export class CommentStore extends Model<CommentInsert, CommentModel> {
       table.integer('like_count').defaultTo(1);
       table.integer('dislike_count').defaultTo(0);
     });
+  }
+
+  async insert(commentInsert: CommentInsert) {
+    const { post_id, parent_id, body, user_id } = commentInsert;
+    const comment = { parent_id, body };
+    let comment_id: uuid; // this will be created by the db
+
+    return this._getConnector().transaction(async (associations) => {
+      const rollbackAssociations = (err: string) => {
+        associations.rollback();
+        this._errorHandler(err);
+      };
+
+      await postgres.transaction(async (entities) => {
+        /** Error handler and rollback for entities */
+        const rollbackEntities = (err: string) => {
+          entities.rollback();
+          this._errorHandler(err);
+        };
+
+        // save the comment
+        await postgres('comments')
+          .transacting(entities)
+          .insert(comment, ['id'])
+          .then((returns) => (comment_id = returns[0].id))
+          .catch(rollbackEntities);
+
+        // increment the comment count for the post by 1
+        await postgres('posts')
+          .transacting(entities)
+          .increment('comment_count', 1)
+          .where({ post_id })
+          .catch(rollbackEntities);
+      });
+
+      // associate the new comment with a user
+      await postgres('user_comments')
+        .transacting(associations)
+        .insert({
+          user_id,
+          comment_id,
+        })
+        .catch(rollbackAssociations);
+
+      // associate the new comment with a post
+      await postgres('post_comments')
+        .transacting(associations)
+        .insert({
+          post_id,
+          comment_id,
+        })
+        .catch(rollbackAssociations);
+    });
+  }
+
+  // !shouldn't be post slug, this is connected data
+  async selectByPostSlug(postSlug: string) {
+    return Promise.resolve([
+      {
+        id: 'id',
+        parentId: null,
+        createdAt: 'some time',
+        body: 'this is the text of the comment',
+        likeCount: 1,
+        dislikeCount: 1,
+      },
+    ]);
   }
 }
 
