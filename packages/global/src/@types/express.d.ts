93d5a39c292cd9fff5fec09550da8fccaa854c5a
utkusarioglu <utkusarioglu@hotmail.com>
2021-02-10 11:07:02 +0300

    Feat: define Express.user by new api

--- packages/global/src/@types/express.d.ts
+++ packages/global/src/@types/express.d.ts
@@ -1,14 +1,12 @@
+import type { User } from 'six__public-api';
+
 /**
  * User properties that will be available to Express.user
  *
  * @todo
  * ! This type shall be derived from UserModel instead of being a standalone type
  */
-export interface UserProps {
-  id: string;
-  username: string;
-  password: string;
-}
+export type UserProps = User['Session']['v1']['Get']['Res']['Success']['body'];
 
 /**
  * Alter user type for express with the user properties for the app
