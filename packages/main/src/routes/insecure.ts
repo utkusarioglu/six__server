9bd683ce715f5d4be32fef4c055b5be911d5163a
utkusarioglu <utkusarioglu@hotmail.com>
2021-02-25 22:10:37 +0300

    Feat: Comment save endpoint in server
    * Comment saving endpoint has been defined with the iife encosure with
    types.

--- packages/main/src/routes/insecure.ts
+++ packages/main/src/routes/insecure.ts
@@ -171,4 +171,42 @@ const router = express.Router();
   );
 })();
 
+(() => {
+  type Method = CommentEndpoint['_save']['_v1'];
+  type Params = Method['_post']['_req']['Params'];
+  type Response = Method['_post']['_res']['Union'];
+  type Endpoint = Method['Endpoint'];
+  type Body = Method['_post']['_req']['Body'];
+
+  router.post<Params, Response, Body>(
+    validateEndpoint<Endpoint>('/comment/save/:requestId'),
+    async (req, res) => {
+      const { requestId } = req.params;
+      const { parentId, body, userId, postId } = req.body;
+      try {
+        const commentSave = await store.comment.insert({
+          body,
+          parentId,
+          userId,
+          postId,
+        });
+
+        res.json({
+          id: requestId,
+          state: 'success' as 'success',
+          body: req.body,
+        });
+      } catch (e) {
+        res.json({
+          id: requestId,
+          state: 'fail' as 'fail',
+          errors: {
+            general: 'COMMENT_SAVE_FAIL',
+          },
+        });
+      }
+    }
+  );
+})();
+
 export default router;
