e9743f03fd79befd81906beb4119e05d038f8b1f
utkusarioglu <utkusarioglu@hotmail.com>
2021-03-16 15:15:39 +0300

    Feat: community accessor create method
    * Community accessor create method has been defined, this allows
    communities to be created from community create route

--- packages/store/src/accessors/communities/communities.accessor.ts
+++ packages/store/src/accessors/communities/communities.accessor.ts
@@ -3,8 +3,13 @@ import type {
   CommunityListOut,
   CommunitySingleOut,
   CommunityCreateOut,
+  CommunityCreateIn,
 } from 'six__server__ep-types';
 import community from '../../models/community/community.model';
+import ucs from '../../models/user-community-subscription/user-community-subscription.model';
+import ucc from '../../models/user-community-creator/user-community-creator.model';
+import postgres from '../../connectors/postgres';
+import ERRORS from '../../errors';
 
 export class CommunitiesAccess {
   list(userId: uuid): Promise<void | CommunityListOut> {
@@ -17,9 +22,38 @@ export class CommunitiesAccess {
     return community.selectForCommunityDetails(communitySlug);
   }
 
-  create(body: any, userId: uuid): Promise<void | CommunityCreateOut> {
-    // temp
-    return Promise.resolve();
+  async create(
+    { description, name, slug }: CommunityCreateIn,
+    userId: uuid
+  ): Promise<void | CommunityCreateOut> {
+    try {
+      const communityId = await postgres.transaction(async (transaction) => {
+        const communityInsertRows = await community.insert(
+          { description, name, slug },
+          transaction
+        );
+        if (!communityInsertRows) {
+          throw new Error(ERRORS.COMMUNITY_INSERT);
+        }
+        const communityId = communityInsertRows[0].id;
+
+        const ucsRows = await ucs.insert({ userId, communityId }, transaction);
+        if (!ucsRows) {
+          throw new Error(ERRORS.UCS_INSERT);
+        }
+
+        const uccRows = await ucc.insert({ userId, communityId }, transaction);
+        if (!uccRows) {
+          throw new Error(ERRORS.UCC_INSERT);
+        }
+
+        return slug;
+      });
+      return this.single(slug);
+    } catch (e) {
+      console.error(e);
+      return Promise.resolve();
+    }
   }
 }
 
